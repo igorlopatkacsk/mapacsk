391770d48bb3dd2cb661f0017272f09c1a32f7c7
Igor Łopatka <igor.lopatka11@gmail.com>
2022-09-01 19:17:05 +0200

    Initial commit

---
+++ map.js
@@ -0,0 +1,5 @@
+var map = L.map('map').setView([52.19891, 20.99793], 18);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
