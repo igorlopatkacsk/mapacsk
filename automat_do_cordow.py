244702a20f6f997608d1a9c2f477d37b097a80cf
AbePersei <marcin-s100@wp.pl>
2022-10-03 11:59:37 +0200

    skrócony kod

--- automat_do_cordow.py
+++ automat_do_cordow.py
@@ -6,18 +6,14 @@ way = (966867056)
 get_nodes = f"https://www.openstreetmap.org/api/0.6/way/{way}.json"
 response = requests.get(get_nodes).text
 response_info = json.loads(response)
-type(response_info)
-elements = response_info["elements"]
-zero = elements[0]
-nodes = zero["nodes"]
+elements = response_info["elements"][0]["nodes"]
 
-for i in nodes:
+for i in elements:
     url = f"https://api.openstreetmap.org/api/0.6/node/{i}.json"
     coord_response = requests.get(url).text
     coord_response_info = json.loads(coord_response)
-    coord_elements = coord_response_info["elements"]
-    zero1 = coord_elements[0]
-    lat = zero1["lat"]
-    lon = zero1["lon"]
+    coord_elements = coord_response_info["elements"][0]
+    lat = coord_elements["lat"]
+    lon = coord_elements["lon"]
     print("["+ str(lat) +','+ str(lon)+"],")
     
