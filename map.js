441dd971135468b534f433e62d818971870ee03d
Igor Lopatka <igor.lopatka@cskmswia.gov.pl>
2022-10-06 14:18:59 +0200

    Czytelniejsza initializacja mapy

--- map.js
+++ map.js
@@ -1,4 +1,5 @@
-var map = L.map('map').setView([52.19891, 20.99793], 18);
+var map = L.map('map', {center: [52.19891, 20.99793], zoom: 17})
+// .setView([52.19891, 20.99793], 18);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '© OpenStreetMap'
